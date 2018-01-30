1d9744e1967406261ef7180bc5edfda21462dd6d
mboichuk <mboichuk@softjourn.com>
2018-01-30 12:18:28 +0200

    Added duplicate order item functionality, edit comment/inscription

---
+++ front/src/components/OrderItem/Comment.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { apiPoint }from '../../constants/server';
+
+export default class Comment extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      show: false,
+      comment: props.comment
+    };
+    this._handleKeyPress = this._handleKeyPress.bind(this);
+  }
+
+  _handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      this.setState({show: false});
+      if(this.props.comment !== this.state.comment) {
+        axios.post(`${apiPoint}/admin/order_item/${this.props.id}/comment`, {
+          comment: this.state.comment
+        }, {
+          headers: {
+            'content-type': 'multipart/form-data'
+          }
+        }).then((response) => {
+          console.log(response);
+        }).catch((e) => {
+          console.log(e);
+        });
+      }
+    }
+  };
+
+  render(){
+    return (
+      <td>
+        {!this.state.show && <span onClick={() => this.setState({show: true})}>{this.state.comment ? this.state.comment : '-'}</span>}
+        {
+          this.state.show &&
+          <input type={"text"} value={this.state.comment || ''} onChange={(e) => this.setState({comment: e.target.value})}
+                 onKeyPress={this._handleKeyPress} />
+        }
+      </td>
+    );
+  }
+}
