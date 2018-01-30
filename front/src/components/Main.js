1d37e1e41bdb7130fb02205490ae842a86b161ba
mboichuk <mboichuk@softjourn.com>
2018-01-30 16:36:45 +0200

    Added activity history view

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router';
 import Orders from './Orders';
 import NotFound from './NotFound';
 import Order from '../containers/OrderContainer';
+import Activity from '../components/OrderItem/Activity';
 
 export default class MainScreen extends Component {
     render () {
@@ -11,6 +12,7 @@ export default class MainScreen extends Component {
                 <Switch>
                     <Route exact path = {'/'} component={Orders} />
                     <Route path = {'/order/:id'} component={Order} />
+                    <Route path = {'/activity/:id'} component={Activity} />
                     <Route component={NotFound} />
                 </Switch>
             </main>
